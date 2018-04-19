ccd7ab711bea80af2a3e9fe63b70a1a0ff1a2c5d
Zachary <zinouye@gmail.com>
2018-04-19 11:17:33 -0700

    updated webpack config, created index.html, and App.jsx file

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const paths = {
   DIST: path.resolve(__dirname, 'dist'),
   SRC: path.resolve(__dirname, 'src'),
-  JS: path.resolve(__dirname, 'src/js'),
+  JS: path.resolve(__dirname, 'src/jsx'),
 };
 
 module.exports = {
