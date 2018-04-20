bece2dc3df235e694ba40b915b04bf1f5da5356d
Zachary <zinouye@gmail.com>
2018-04-20 10:42:57 -0700

    installed babel-preset-react

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -1,1 +1,15 @@
-console.log('hello');
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+
+export default class Hello extends Component {
+  render() {
+    return (
+      <div>
+        Hello from react
+      </div>
+    );
+  }
+}
+
+render(<Hello />, document.getElementById('app'));
