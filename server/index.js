2df3dedded225d2cf8aefc61e1cecb5d9aac26c6
Zachary <zinouye@gmail.com>
2018-04-21 12:49:30 -0700

    added server and serve script to packagejson

--- server/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-
-app.use(path.join(__dirname, dist));
-
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
-})
